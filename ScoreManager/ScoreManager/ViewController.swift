5c5873d9a59094c380fcd5d97cb74c5f910b595a
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2018-11-15 11:07:52 +0900

    init commit ScoreManager

---
+++ ScoreManager/ScoreManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreManager
+//
+//  Created by ken miyahira on 2018/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
