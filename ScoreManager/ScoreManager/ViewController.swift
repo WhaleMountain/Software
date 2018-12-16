37ccb16f14ba47845eeabb6f0b2277af7144e799
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2018-12-16 13:40:20 +0900

    pull request test

--- ScoreManager/ScoreManager/ViewController.swift
+++ ScoreManager/ScoreManager/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        var test:Int // pull requestのテスト用で追加
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
