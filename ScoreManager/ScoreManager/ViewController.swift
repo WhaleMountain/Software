7b6b4ba26682751ce77097709e39b85d4c59bfae
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2018-12-16 14:52:27 +0900

    pull requ test

--- ScoreManager/ScoreManager/ViewController.swift
+++ ScoreManager/ScoreManager/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         var test:Int // pull requestのテスト用で追加
         var test2:Int // git test
+        var test3:Int // pull request テスト
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
